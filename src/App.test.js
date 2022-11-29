b3049c701455df65c537fea9fed856ec22d3a5ba
Rihards Simanovics <rihards.s@griffin-web.studio>
2022-11-29 02:42:07 +0000

    2.0.4 (bug-bash) | bug bashing (removed all warnings), small test?

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Production Site URL label', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Production Site URL/i);
   expect(linkElement).toBeInTheDocument();
 });
