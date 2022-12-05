c80e2a0f1e792ea0a0225562b6ff26ca4290a866
Rihards Simanovics <rihards.s@griffin-web.studio>
2022-12-05 14:56:58 +0000

    2.0.6 | UI fixes, Initial logic, build fix

--- src/componenets/Widget/SpecImage.js
+++ src/componenets/Widget/SpecImage.js
@@ -3,6 +3,6 @@ import React from "react";
 export default function SpecImage(props) {
 
     return (
-        <div class="gwssc-image invert"><img src="" alt="Spec Image Overlay" class="image"/></div>
+        <div class="gwssc-image invert"><img src="" alt="Spec Overlay" class="image"/></div>
     )
 }
