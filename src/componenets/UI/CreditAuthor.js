1cd98d7200465d9286626925db9c2c454a813ca3
Rihards Simanovics <rihards.s@griffin-web.studio>
2022-12-31 14:34:02 +0000

    2.4.5 | sass files cleanup, redesign of credits

--- src/componenets/UI/CreditAuthor.js
+++ src/componenets/UI/CreditAuthor.js
@@ -2,20 +2,14 @@ export default function CreditAuthor(props) {
     const { projectUrl, projectName, projectLicense, projectAuthor } = props;
 
     return (
-        <div className="gwssc__credit-author">
-            <p className="gwssc__credit-author-project-name">
-                <a href={projectUrl} className="gwssc__credit-author-url" target="_blank" rel="noopener noreferrer">
+        <tr className="gwssc-credit">
+            <td className="gwssc-credit__project-name">
+                <a href={projectUrl} className="gwssc-credit__author" target="_blank" rel="noopener noreferrer">
                     {projectName}
                 </a>
-            </p>
-            <ul className="gwssc__credit-author-meta">
-                <li>
-                    license - <span className="gwssc__credit-author-license">{projectLicense}</span>
-                </li>
-                <li>
-                    Copyright - <span className="gwssc__credit-author-name">{projectAuthor}</span>
-                </li>
-            </ul>
-        </div>
+            </td>
+            <td className="gwssc-credit__author-license">{projectLicense}</td>
+            <td className="gwssc-credit__author-name">{projectAuthor}</td>
+        </tr>
     );
 }
--- src/componenets/Widget/InputGroups/Credits.js
+++ src/componenets/Widget/InputGroups/Credits.js
@@ -17,29 +17,27 @@ export default function SpecCheckerCredits(props) {
                         <p>This software uses the following open source projects:</p>
 
                         <div className="gwssc-input-wrap gwssc-input-wrap--radius-top gwssc-input-wrap--radius-bottom">
-                            <ul>
-                                <li>
+                            <table className="gwssc-credits">
+
+                                <thead className="gwssc-credits__header">
+                                    <tr>
+                                        <th>Project</th>
+                                        <th>License</th>
+                                        <th>Author</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody className="gwssc-credits__body">
                                     <CreditAuthor projectName="ReactJS" projectAuthor="Facebook, Inc. and its affiliates" projectLicense="MIT" projectUrl="https://reactjs.org/" />
-                                </li>
-                                <li>
                                     <CreditAuthor projectName="CodeMirror" projectAuthor="Marijn Haverbeke and others" projectLicense="MIT" projectUrl="https://codemirror.net/" />
-                                </li>
-                                <li>
                                     <CreditAuthor projectName="React-Codemirror" projectAuthor="[UIW]" projectLicense="MIT" projectUrl="https://uiwjs.github.io/react-codemirror/" />
-                                </li>
-                                <li>
                                     <CreditAuthor projectName="Fantasticon" projectAuthor="Tancredi Trugenberger" projectLicense="MIT" projectUrl="https://www.npmjs.com/package/fantasticon/" />
-                                </li>
-                                <li>
                                     <CreditAuthor projectName="Jest" projectAuthor="Facebook, Inc. and its affiliates" projectLicense="MIT" projectUrl="https://jestjs.io/" />
-                                </li>
-                                <li>
                                     <CreditAuthor projectName="Sass" projectAuthor="Sass Core Team" projectLicense="MIT" projectUrl="https://sass-lang.com/" />
-                                </li>
-                                <li>
                                     <CreditAuthor projectName="Web Vitals" projectAuthor="Google LLC" projectLicense="MIT" projectUrl="https://web.dev/vitals/" />
-                                </li>
-                            </ul>
+                                </tbody>
+
+                            </table>
                         </div>
 
                         <p>We would like to express our gratitude to the authors and contributors of these projects for their work.</p>
